8956f5f472df3e2491c4845f8a2ab756c8bb765e
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-23 22:41:36 +0900

    Create app.js

---
+++ week1/section2/Post 요청 처리/app.js
@@ -0,0 +1,27 @@
+var express = require('express')    //express 모듈 불러오기
+var app = express()
+var bodyParser = require('body-parser')
+
+app.listen(3000, function(){    //3000이란 포트를 기반으로 function 함수 실행
+    console.log("start!! express server on port 3000!");
+});  
+
+app.use(express.static('public'))   //public을 staic 디렉토리로 정해놓음
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true})) 
+
+//url routing 
+app.get('/', function(req,res){ //콜백함수 (비동기)
+    console.log('test');
+    res.sendFile(__dirname + "/main.html")
+})
+
+app.get('/main', function(req,res){ 
+    res.sendFile(__dirname + "/main.html")
+})
+
+app.post('/email_post', function(req, res){
+    console.log(req.body.email) //적은 이메일 가져오기
+    res.send("<h1>welcome "+req.body.email+"</h1>")
+    res.send("post response")
+})
