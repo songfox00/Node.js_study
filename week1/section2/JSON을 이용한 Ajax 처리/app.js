ba2f5cb980ee811e14e66dd7a856ac7ea99f9835
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-26 23:11:06 +0900

    Create app.js

---
+++ week1/section2/JSON을 이용한 Ajax 처리/app.js
@@ -0,0 +1,35 @@
+var express = require('express')    //express 모듈 불러오기
+var app = express()
+var bodyParser = require('body-parser')
+
+app.listen(3000, function(){    //3000이란 포트를 기반으로 function 함수 실행
+    console.log("start!! express server on port 3000!");
+});  
+
+app.use(express.static('public'))   //public을 staic 디렉토리로 정해놓음
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true})) 
+app.set('view engine', 'ejs') //view engine는 ejs야
+
+//url routing 
+app.get('/', function(req,res){ //콜백함수 (비동기)
+    console.log('test');
+    res.sendFile(__dirname + "/main.html")
+})
+
+app.get('/main', function(req,res){ 
+    res.sendFile(__dirname + "/main.html")
+})
+
+app.post('/email_post', function(req, res){
+    console.log(req.body.email) //적은 이메일 가져오기
+    //res.send("<h1>welcome "+req.body.email+"</h1>")
+    //res.send("post response")
+    res.render('email.ejs', {'email' : req.body.email}) //ejs에 email이란 네임 값을 찾아서 req.body.email 값으로 치환해서 브라우저에 응답
+})
+
+app.post('/ajax_send_email', function(req,res){
+    console.log(req.body.email);
+    var responseData={'result':'ok', 'email':req.body.email};
+    res.json(responseData)
+})
