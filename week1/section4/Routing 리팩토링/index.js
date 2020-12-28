c5b109955c3b29879f995289840128a156480238
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-28 14:51:49 +0900

    Create index.js

---
+++ week1/section4/Routing 리팩토링/index.js
@@ -0,0 +1,17 @@
+var express = require('express')    //express 모듈 불러오기
+var app = express()
+var router = express.Router();
+var path=require('path'); //상대경로
+
+var main= require('./main/main')
+var email= require('./email/email')
+
+//url routing 
+router.get('/', function(req,res){ //콜백함수 (비동기)
+    res.sendFile(path.join(__dirname , '../public/main.html'))
+})
+
+router.use('/main', main) //main으로 들어오면 main.js를 씀
+router.use('/email', email)
+
+module.exports=router;
