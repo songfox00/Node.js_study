5ded8f9e4bee81a918e13107463ac2f7d0326b51
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2020-12-28 15:07:42 +0900

    Create index(join).js
    join 폴더 안

---
+++ week1/section 5/DB에 데이터 추가 1편/index(join).js
@@ -0,0 +1,24 @@
+var express = require('express')    //express 모듈 불러오기
+var app = express()
+var router = express.Router();
+var path=require('path'); //상대경로
+var mysql=require('mysql')
+
+// DATABASE SETTING
+var connection=mysql.createConnection({
+    host : 'localhost',
+    port : 3306,
+    user : 'root',
+    password : 'LunMoon2423',
+    database : 'jsman'
+})
+
+connection.connect()
+
+// ROUTER
+router.get('/', function(req, res){
+    console.log('get join url');
+    res.sendFile(path.join(__dirname, '../../public/join.html'))
+})
+
+module.exports=router;
